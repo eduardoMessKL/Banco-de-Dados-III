8bc60890025b2cc3624e1c4326796257b3434c19
Eduardo Messias Kulka <127259832+eduardoMessKL@users.noreply.github.com>
2023-07-18 09:48:10 -0300

    Add files via upload

---
+++ meusgastos/domain/service/UsuarioService.java
@@ -0,0 +1,98 @@
+package com.example.meusgastos.domain.service;
+
+import java.util.Date;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import com.example.meusgastos.domain.dto.usuario.UsuarioRequestDTO;
+import com.example.meusgastos.domain.dto.usuario.UsuarioResponseDTO;
+import com.example.meusgastos.domain.exception.ResourceBadRequestException;
+import com.example.meusgastos.domain.exception.ResourceNotFoundException;
+import com.example.meusgastos.domain.model.Usuario;
+import com.example.meusgastos.domain.repository.UsuarioRepository;
+
+@Service
+public class UsuarioService implements ICRUDService<UsuarioRequestDTO, UsuarioResponseDTO> {
+    @Autowired
+    private UsuarioRepository usuarioRepository;
+    @Autowired
+    private ModelMapper mapper;
+    @Autowired
+    private BCryptPasswordEncoder passwordEncoder;
+
+    @Override
+    public List<UsuarioResponseDTO> obterTodos() {
+        List<Usuario> usuarios = usuarioRepository.findAll();
+        return usuarios.stream()
+        .map(usuario -> mapper.map(usuario, UsuarioResponseDTO.class))
+        .collect(Collectors.toList());
+    }
+
+    @Override
+    public UsuarioResponseDTO obterPorId(Long id) {
+        Optional<Usuario> optUsuario = usuarioRepository.findById(id);
+        if(optUsuario.isEmpty()){
+            throw new ResourceNotFoundException("Não foi possivel encontrar o usuário com o id: " + id);
+        }
+        return mapper.map(optUsuario.get(), UsuarioResponseDTO.class);
+    }
+
+    @Override
+    public UsuarioResponseDTO cadastrar(UsuarioRequestDTO dto) {
+        if(dto.getEmail() == null || dto.getSenha() == null){
+            throw new ResourceBadRequestException("Email e senha são obrigatorios!");
+        }
+
+        Optional <Usuario> optUsuario = usuarioRepository.findByEmail(dto.getEmail());
+        if(optUsuario.isPresent()){
+            throw new ResourceBadRequestException("Ja existe um usuario cadastrado com esse email: " + dto.getEmail());
+        }
+
+        Usuario usuario = mapper.map(dto, Usuario.class);   
+        usuario.setDataCadastro(new Date());
+        //Encriptar a senha
+        String senha = passwordEncoder.encode(usuario.getSenha());
+        usuario.setSenha(senha);
+        usuario.setId(null);
+        usuario = usuarioRepository.save(usuario);
+        return mapper.map(usuario, UsuarioResponseDTO.class);
+
+    }
+
+    @Override
+    public UsuarioResponseDTO atualizar(Long id, UsuarioRequestDTO dto) {
+        UsuarioResponseDTO usuarioBanco = obterPorId(id);
+        if(dto.getEmail() == null || dto.getSenha() == null){
+            throw new ResourceBadRequestException("Email e senha são obrigatorios!");
+        }
+        Usuario usuario = mapper.map(dto, Usuario.class);
+        usuario.setId(id);
+        usuario.setDataCadastro(usuarioBanco.getDataCadastro());
+        usuario.setDataInativacao(usuarioBanco.getDataInativacao());
+        usuario = usuarioRepository.save(usuario);
+        return mapper.map(usuario, UsuarioResponseDTO.class);
+    }
+
+    @Override
+    public void deletar(Long id) {
+        //Apagar usuario da base:
+        //obterPorId(id);
+        //usuarioRepository.deleteById(id);
+
+        //Inativar usuario:
+        Optional<Usuario> optUsuario = usuarioRepository.findById(id);
+        if(optUsuario.isEmpty()){
+            throw new ResourceNotFoundException("Não foi possivel encontrar o usuário com o id: " + id);
+        }
+        Usuario usuario = optUsuario.get();
+        usuario.setDataInativacao(new Date());
+        usuarioRepository.save(usuario);
+    }
+    
+}
